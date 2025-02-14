c57772d9be35c6a79d364d636f4acd19617f6457
rohan-5783 <137896572+rohan-5783@users.noreply.github.com>
2025-02-14 10:05:57 +0530

    update

---
+++ Practical Exam - 01/Practical Exam 2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int row,col,space,i,n;
+	
+	cout << " Enter the number of rows:";
+	cin >> n;
+	
+	for ( int i=1; i<=n; i++)
+	{
+		
+		for(row=col;row<=1;row++)
+		{
+			
+		}
+	
+	}
+	
+}
